407c686cf39a38bd933cee7327de801ace14488f
Nikita Bosoi <nikitabosoi@yandex.ru>
2022-07-26 17:43:00 +0300

    adding all files

---
+++ src/test/java/locators_info/OrderPageLocators.java
@@ -0,0 +1,8 @@
+package locators_info;
+
+public class OrderPageLocators {
+
+    public void print(){
+        System.out.println("xxx");
+    }
+}
---
+++ src/test/java/tests/TestingQA.java
@@ -0,0 +1,41 @@
+package tests;
+
+import locators_info.MainPageMethods;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class TestingQA {
+
+    MainPageMethods mainPageMethods = new MainPageMethods();
+
+    @Before
+    public void setUp(){
+        mainPageMethods.setUp();
+    }
+
+    @Test
+    public void testingQA1(){
+        mainPageMethods.testDropDownQuestionAnswer1();
+    }
+    @Test
+    public void testingQA2() { mainPageMethods.testDropDownQuestionAnswer2(); }
+    @Test
+    public void testingQA3() { mainPageMethods.testDropDownQuestionAnswer3(); }
+    @Test
+    public void testingQA4() { mainPageMethods.testDropDownQuestionAnswer4(); }
+    @Test
+    public void testingQA5() { mainPageMethods.testDropDownQuestionAnswer5(); }
+    @Test
+    public void testingQA6() { mainPageMethods.testDropDownQuestionAnswer6(); }
+    @Test
+    public void testingQA7() { mainPageMethods.testDropDownQuestionAnswer7(); }
+    @Test
+    public void testingQA8() { mainPageMethods.testDropDownQuestionAnswer8(); }
+
+    @After
+    public void tearDown(){
+        mainPageMethods.tearDown();
+    }
+}
